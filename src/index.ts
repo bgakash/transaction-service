dfd9c144fd7e807195ea586b3ad871cbc381d57d
Akash <akash.ganapathi@photon.com>
2025-03-27 12:51:18 +0530

    basic server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
-import { client } from "./db";
+import path from "path";
+import { readFile } from "fs/promises";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
+app.get("/transactions/:offset", async (req, res) => {
+  const offset = req.params.offset;
   try {
-    const result = await client.query("SELECT * FROM payments");
-    res.json(result.rows);
+    const filePath = path.join(__dirname, "./data/transaction.json");
+    const fileContent = await readFile(filePath, "utf-8");
+    const jsonData = JSON.parse(fileContent);
+    res.json(jsonData.slice(0, offset));
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Database error");
+    console.error("Error reading JSON file:", err);
+    res.status(500).send("Failed to read JSON file");
   }
 });
 
